95de0b747d2eabc0ad9431a272ee4157bfc39286
Nguyen Van Dung <86200342+dungkpht123@users.noreply.github.com>
2023-12-03 13:36:13 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql2');
     
 var conn = mysql.createConnection({
- host: "localhost",
- user: "root",
- password: "123456",
- database: "nodejsck"
+ host: "ba0a4injihadtbrgblrr-mysql.services.clever-cloud.com",
+ user: "ullrzjgknbnmhltm",
+ password: "FAxaAzLHTTw37HTJOdud",
+ database: "ba0a4injihadtbrgblrr"
 });
 conn.connect();
 module.exports = conn;
